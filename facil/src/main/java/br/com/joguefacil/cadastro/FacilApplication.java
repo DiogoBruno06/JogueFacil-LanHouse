0ba99df81d1458dd6203813e26ebc404ba2da2a3
JoaoLuis03 <diogobruno06@outlook.com>
2025-08-20 15:03:40 -0300

    Criando cluster para cada aplicação funcional e fix nas configs de banco de dados

--- facil/src/main/java/br/com/joguefacil/cadastro/FacilApplication.java
+++ facil/src/main/java/br/com/joguefacil/cadastro/FacilApplication.java
@@ -2,10 +2,8 @@ package br.com.joguefacil.cadastro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class FacilApplication {
 
 	public static void main(String[] args) {
